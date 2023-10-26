0fdd8842f58380fb6706101a08547214e888d53a
Scecil044 <scecil072@gmail.com>
2023-10-26 17:23:48 +0300

    after resolving path.json

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.use("/api/companies", companyRoutes);
 //hosting configurations
 app.use(express.static(path.join(__dirname, "/client/dist")));
 app.get("*", (req, res) => {
-  res.sendFile(path.json(__dirname, "client", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
 //middleware
